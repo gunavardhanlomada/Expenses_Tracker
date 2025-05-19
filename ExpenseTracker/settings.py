69e36c4414579c23d3e2abb2b4a12e9da59ff310
Guna Vardhan Reddy <138978462+gunavardhanlomada@users.noreply.github.com>
2025-05-19 16:28:20 +0530

    Update settings.py

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -135,8 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '1747503gunavardhanreddy@gmail.com'
-EMAIL_HOST_PASSWORD = 'rgclvjgfvwgcibac'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/img')
 MEDIA_URL = '/img/'
